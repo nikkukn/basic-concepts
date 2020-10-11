b13c1a8d82f3df2121853909a2975ac0da9471ce
buckleader <jasmin28virdi@gmail.com>
2020-10-11 19:18:06 +0530

    tricky javascript expressions

---
+++ JS/javascriptExpressions.js
@@ -0,0 +1,13 @@
+/*
+What does the below expressions evaluate to
+*/
+
+console.log(1 + true) // results in 2
+
+console.log(true + true) // returns 2
+
+console.log("1" + true) // returns 1true
+
+console.log('2' > '3') // returns false as it compares the string ASCII value
+
+console.log('two' > 'three') // return complete string value. It will compare the value of each character in the string.
