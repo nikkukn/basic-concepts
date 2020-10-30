d8a8c1e99366d3c445186af9ce4ec3866465b18d
buckleader <jasmin28virdi@gmail.com>
2020-10-31 03:23:36 +0530

    bubble sort

---
+++ SortingAlgorithms/bubbleSort.js
@@ -0,0 +1,22 @@
+/*
+Bubble sort algorithm
+*/
+
+
+let A = [11, 12, 22, 25, 34, 64, 90]
+
+function BubbleSort(A) {
+
+    for (let i = 0; i < A.length; i++) {
+
+        for (let j = 0; j < A.length - i - 1; j++) {
+
+            if (A[j] > A[j + 1]) {
+                let temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
+        }
+
+    }
+}
