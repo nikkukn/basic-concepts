b8377c85cd52cb3718aa729ab2e93da0d6d6d4c6
buckleader <jasmin28virdi@gmail.com>
2020-10-31 03:43:05 +0530

    bubble sort fixes

--- SortingAlgorithms/bubbleSort.js
+++ SortingAlgorithms/bubbleSort.js
@@ -9,7 +9,7 @@ function BubbleSort(A) {
 
     for (let i = 0; i < A.length; i++) {
 
-        for (let j = 0; j < A.length - i - 1; j++) {
+        for (let j = 0; j < A.length - i; j++) {
 
             if (A[j] > A[j + 1]) {
                 let temp = A[j];
