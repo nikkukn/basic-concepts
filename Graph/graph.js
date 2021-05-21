f64075cb03e683a5c366d875b45635b51e50c597
Jasmin2895 <jasmin28virdi@gmail.com>
2021-05-22 04:08:43 +0530

    graph implementation in javascript

---
+++ Graph/graph.js
@@ -0,0 +1,44 @@
+class Node {
+    constructor(data) {
+        this.value = data;
+        this.edges = []; //connection of each node of the graph
+    }
+}
+
+
+class Graph {
+    constructor(directed=false){
+        this.unidirected = directed;
+        this.nodes = [];
+    }
+
+    // add Graph node and push it to the nodes array
+    addValue(value) {
+        this.nodes.push(new Node(value));
+    }
+
+    // removing a node from the graph
+    removeNode(value) {
+        // remove node from the node from the graph
+        this.nodes = this.nodes.filter(node => node.value !== value)
+        // remove edges from the Node for the removed value
+        this.nodes.forEach(node=> {
+            node.edges = node.edges.filter(edge=> edge.value!==value)
+        });
+    }
+
+    // get a node from linked list
+    getNode(value) {
+        return this.nodes.find(node=>node.value===value)
+    }
+
+    // Adding an edge between two nodes
+    addEdge(value1, value2) {
+        const node1 = this.getNode(value1)
+        const node2 = this.getNode(value2)
+        node1.edges.push(node2)
+        if(this.unidirected) 
+            node2.edges.push(node1)
+        return `An edge between ${node1.value} and ${node2.value} was added`;
+    }
+}
