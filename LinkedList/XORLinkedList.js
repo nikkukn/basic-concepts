b9881be064da6462723eff229f74aed7dd6e6bdb
Jasmin2895 <jasmin28virdi@gmail.com>
2021-05-21 15:44:09 +0530

    XOR linked list

---
+++ LinkedList/XORLinkedList.js
@@ -0,0 +1,37 @@
+class Node {
+    constructor(element) {
+        this.element = element;
+        this.npx = null;
+        this.prev = null;
+    }
+}
+
+class LinkedList {
+    constructor(props) {
+        this.head = null;
+        this.size = 0;
+    }
+
+    // XOR the element
+    XOR(node1, node2) {
+        return node1 ^ node2;
+    }
+
+    // inserting into XOR Linked List
+    insert() {
+        let newNode = new Node(1);
+        newNode.npx = XOR(this.head, null);
+        // logic to update the next pointer of the previous element once the next element is being set.
+        if(this.head !== null) {
+            let next = XOR(this.head.npx, null);
+            this.head.npx = XOR(newNode, next)
+        }
+
+        // change the head node to the last node
+        this.head = newNode;
+    }
+
+    createXORLinkedList() {
+
+    }
+}
