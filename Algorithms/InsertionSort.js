e88bfa5c667b5335f827014cd0603c8a4393d054
Jasmin2895 <jasmin28virdi@gmail.com>
2021-05-23 01:41:40 +0530

    insertion sort

---
+++ Algorithms/InsertionSort.js
@@ -0,0 +1,12 @@
+// function write insertion sorting in JS
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      for (let j = 0; j < i; j++) {
+        if (arr[i] < arr[j]) {
+          const [item] = arr.splice(i, 1)
+          arr.splice(j, 0, item)
+        }
+} }
+return arr 
+}
