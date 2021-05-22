b2487ab587175ea482feae89c8fbbba57f250a6e
Jasmin2895 <jasmin28virdi@gmail.com>
2021-05-22 21:02:42 +0530

    bubble sort using do while

--- Algorithms/BubbleSort.js
+++ Algorithms/BubbleSort.js
@@ -14,3 +14,23 @@ function bubbleSort(array) {
 }
 
 // Time Complexity - O(n^2)
+
+
+// Bubble sort with a do while loop
+
+function bubbleSortWithDoWhile(arr) {
+    let index = 0;
+    let swapped = false;
+    do {
+        swapped = false;
+        arr.forEach((element, index) => {
+            if(element > arr[index+1]) {
+                let temp = arr[index+1];
+                arr[index+1]= element;
+                element = temp;
+                swapped = true;
+            }
+        });
+    }while(swapped)
+    return arr;
+}
