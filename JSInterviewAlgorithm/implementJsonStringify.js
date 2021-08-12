f1eaa1287672bc6f83f2bbca911efaa92e8e1b9a
Jasmin2895 <jasmin28virdi@gmail.com>
2021-08-13 01:15:25 +0530

    js questions

---
+++ JSInterviewAlgorithm/implementJsonStringify.js
@@ -0,0 +1,70 @@
+// Q - Implement JSON.stringify from scratch
+
+function stringify(data) {
+
+    if(typeof data === 'bigint'){
+        throw new Error('Do not know how to serialize a BigInt at JSON.stringify')
+    }
+
+    if(typeof data ===  'string'){
+        return `"${data}"`;
+    }
+
+    if(typeof data === 'function'){
+        return undefined;
+    }
+
+    if(data!==data){
+        return "null";
+    }
+
+    if(data === Infinity){
+        return "null"
+    }
+
+    if(data === -Infinity){
+        return "null";
+    }
+
+    if(typeof data === 'number'){
+        return `${data}`;
+    }
+
+    if(typeof data === "boolean"){
+        return `${data}`;
+    }
+
+    if(data === null){
+        return 'null';
+    }
+
+    if(data === undefined){
+        return 'null';
+    }
+
+    if(typeof data === 'symbol'){
+        return 'null';
+    }
+
+    if(data instanceof Date){
+        return `"${data.toISOString()}"`;
+    }
+
+    if(Array.isArray(data)){
+        const tempData = data.map((ele)=> stringify(ele));
+        return `[${tempData.join(",")}]`
+    }
+
+    if(typeof data === 'object'){
+      const arr = Object.entries(data).reduce((acc, [key, value])=> {
+        if(value === undefined){
+            return acc;
+        }
+
+        acc.push(`"${key}": ${stringify(value)}`);
+        return acc;
+      },[]);
+
+      return `{${arr.join(",")}}`;
+  }
+} 
