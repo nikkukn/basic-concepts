c1f1f899f2f5182c74990e421a4b86d59aab816f
Jasmin2895 <jasmin28virdi@gmail.com>
2021-08-14 17:33:26 +0530

    js questions

--- JSInterviewAlgorithm/implementJSONParse.js
+++ JSInterviewAlgorithm/implementJSONParse.js
@@ -1,54 +1,43 @@
 // Q - Implement Json.parse on your own ?
 
 function parse(str){
-    if(str===""){
+    if(str === '') {
         throw Error();
-    }
-
-    if(str[0]==="'"){
+      }
+      if(str[0] === "'") {
         throw Error();
-    }
-
-    if(str === 'null'){
+      }
+      if(str === 'null') {
         return null;
-    }
-
-    if(str === '{}'){
+      }
+      if(str === '{}') {
         return {};
-    }
-
-    if(str === '[]'){
+      }
+      if(str === '[]') {
         return [];
-    }
-
-    if(str === 'true'){
+      }
+      if(str === 'true') {
         return true;
-    }
-
-    if(str === 'false'){
+      }
+      if(str === 'false') {
         return false;
-    }
-
-    if(str === '"'){
-        return str.slice(1,-1)  // removes first and last element
-    }
-
-    if(+str===+str){
+      }
+      if(str[0] === '"') {
+        return str.slice(1, -1);
+      }
+      if(+str === +str) {
         return Number(str);
-    }
-
-    if(str[0] === "{"){
-        return str.slice(1,-1).split(",").reduce((acc, item)=> {
-            const index = item.indexOf(':');
-            const key = item.slice(0, index);
-            const value = item.slice(index+1);
-            acc[parse(key)] = parse(value);
-
-            return acc;
-        }, {})
-    }
-
-    if(str[0] === '['){
-        return str.slice(1,-1).split(',').map((ele)=>parse(ele))
-    }
+      }
+      if(str[0] === '{') {
+        return str.slice(1, -1).split(',').reduce((acc, item) => {
+          const index = item.indexOf(':');
+          const key = item.slice(0, index)
+          const value = item.slice(index + 1);
+          acc[parse(key)] = parse(value);
+          return acc;
+        }, {});
+      }
+      if(str[0] === '[') {
+        return str.slice(1, -1).split(',').map((value) => parse(value));
+      }
 }
