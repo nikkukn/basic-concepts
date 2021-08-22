b96002e0c1efb896a5029cf7ebd7a058af3a0cf4
Jasmin2895 <jasmin28virdi@gmail.com>
2021-08-23 00:32:17 +0530

    dutch national flag

---
+++ JSInterviewAlgorithm/dutchNationalFlagProblem.js
@@ -0,0 +1,8 @@
+/* 
+Q - The Dutch national flag (DNF) problem is one of the most popular programming problems proposed by Edsger Dijkstra. The flag of the Netherlands consists of three colors: white, red, and blue. The task is to randomly arrange balls of white, red, and blue such that balls of the same color are placed together.
+*/
+
+
+function dutchNationalFlagProblem(){
+    
+}
